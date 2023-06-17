e2960968f29e3aa86c36f4d23446ce73c871a4d1
Gustavo-Rubio <grubio3796@gmail.com>
2023-06-17 01:44:41 -0700

    WMO SVG File

--- weather_api.py
+++ weather_api.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
 
     lat_long_url_search = (f'https://api.open-meteo.com/v1/dwd-icon?latitude={lat}&longitude={longi}&hourly=temperature_2m')
     
-    city_url_search = f'https://geocoding-api.open-meteo.com/v1/search?name=New+York&count=10&language=en&format=json'
+    city_url_search = (f'https://geocoding-api.open-meteo.com/v1/search?name=New+York&count=10&language=en&format=json')
+    
     
     response = requests.get(lat_long_url_search)
     result = response.json()
