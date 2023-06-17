b1b94e4f3e89cdd1892de686d48db1ecc0f45d36
Jenny Cai <jxcai@mit.edu>
2023-06-16 23:12:19 -0400

    add basic settings + style sheet

---
+++ weather-forecasting/tailwind.config.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    './src/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{scss,module.scss}',
+  ],
+  theme: {
+    screens: {
+      mobile: '370px',
+      desktop: '692px',
+      'real-desktop': '1025px'
+    },
+    colors: {
+      white: '#FFFFFF',
+      black: '#35495e',
+      blue: '#8ecfe0',
+      'blue-dark': '#00acc1',
+      'gray-light': '#efefef',
+      'gray-dark': '#607d8b'
+    },
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ weather-forecasting/vite.config.ts
@@ -0,0 +1,49 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+import react from '@vitejs/plugin-react';
+
+const root: string = resolve(__dirname, 'src');
+const outDir: string = resolve(__dirname, 'dist');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root,
+  base: '/weather-app/',
+  resolve: {
+    alias: [
+      { find: '@/Components', replacement: resolve(__dirname, 'src/components') },
+      { find: '@/SCSS', replacement: resolve(__dirname, 'src/scss') },
+    ],
+  },
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+    })
+  ],
+  build: {
+    outDir,
+    assetsInlineLimit: 0,
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'js/main.js',
+        chunkFileNames: 'js/[name].js',
+        assetFileNames: ({ name = '' }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name)) {
+            return 'img/[name].[ext]';
+          }
+          if (/\.css$/.test(name)) {
+            return 'css/[name].[ext]';
+          }
+          if (/manifest\.json$/.test(name)) {
+            return '[name].[ext]';
+          }
+          return '[name].[ext]';
+        }
+      }
+    },
+  }
+});
